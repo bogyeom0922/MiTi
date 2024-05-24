f7732d4b0214ba2535d9dc703f7d0607a8cae751
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2024-05-24 03:42:38 +0000

    feat: 클라이언트 자격 증명

--- crawling/spotify.py
+++ crawling/spotify.py
@@ -4,3 +4,4 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 client_id = '552a87573e13448e934f2c843c11c6a5'
 client_secret = '3823736d49a94614912d2da790d0a808' 
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret) #인증
