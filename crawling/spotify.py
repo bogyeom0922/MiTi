dd9d5186b74b544eadd0e8b2441ffdc8a443d997
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2024-05-24 03:43:08 +0000

    feat: Spotify 객체 생성

--- crawling/spotify.py
+++ crawling/spotify.py
@@ -4,4 +4,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 client_id = '552a87573e13448e934f2c843c11c6a5'
 client_secret = '3823736d49a94614912d2da790d0a808' 
+
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret) #인증
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
