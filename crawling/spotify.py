f08f32cca78b93d2ed6fc53336de43a80dc0b838
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2024-05-24 03:43:48 +0000

    feat: csv파일 경로 지정

--- crawling/spotify.py
+++ crawling/spotify.py
@@ -5,7 +5,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_id = '552a87573e13448e934f2c843c11c6a5'
 client_secret = '3823736d49a94614912d2da790d0a808' 
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret) #인증
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+csv_file_path = "spotify_tracks.csv"
