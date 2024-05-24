6aacd4cb4785acfa6e13f004d85420747be1307d
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2024-05-24 03:41:54 +0000

    feat: spotify api 자격 증명

---
+++ crawling/spotify.py
@@ -0,0 +1,6 @@
+import spotipy
+import csv
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_id = '552a87573e13448e934f2c843c11c6a5'
+client_secret = '3823736d49a94614912d2da790d0a808' 
