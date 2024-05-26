c9ad9addde8211a06a3fe6b7f784953043fe72f2
bogyeom0922 <pbk0398@gmail.com>
2024-05-26 13:58:38 +0900

    feat: model 추가

--- src/main/java/com/MiTi/MiTi/controller/AlbumController.java
+++ src/main/java/com/MiTi/MiTi/controller/AlbumController.java
@@ -1,6 +1,7 @@
 package com.MiTi.MiTi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +13,9 @@ public class AlbumController {
     }
 
     @GetMapping("/album")
-    public String album_detail() {
+    public String album_detail(Model model) {
+
+        model.addAttribute("album");
         return "album/album_detail";
     }
 
