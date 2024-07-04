4188db39443300ebae8ca21c0291d2c6e86b1c82
bogyeom0922 <pbk0398@gmail.com>
2024-07-05 01:16:12 +0900

    feat: 테이블 명 지정

--- src/main/java/com/MiTi/MiTi/entity/Comment.java
+++ src/main/java/com/MiTi/MiTi/entity/Comment.java
@@ -1,9 +1,11 @@
 package com.MiTi.MiTi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
+@Table(name = "user_comment")
 public class Comment {
 
 }
