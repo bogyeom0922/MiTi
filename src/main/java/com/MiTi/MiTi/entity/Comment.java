c57a31978ec572cc4595d22aca55fa6f337091f4
bogyeom0922 <pbk0398@gmail.com>
2024-07-05 01:16:59 +0900

    feat: 기본키값 지정

--- src/main/java/com/MiTi/MiTi/entity/Comment.java
+++ src/main/java/com/MiTi/MiTi/entity/Comment.java
@@ -1,11 +1,12 @@
 package com.MiTi.MiTi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Table(name = "user_comment")
 public class Comment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
