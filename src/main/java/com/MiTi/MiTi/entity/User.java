ec1e2d52b88e0f5302a4c61734cac9c224e3334b
bogyeom0922 <pbk0398@gmail.com>
2024-07-05 16:05:14 +0900

    feat: 기본값 지정

--- src/main/java/com/MiTi/MiTi/entity/User.java
+++ src/main/java/com/MiTi/MiTi/entity/User.java
@@ -1,7 +1,13 @@
 package com.MiTi.MiTi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
