d7dae6fb0a3767b7978ff8baf250471cceab9103
bogyeom0922 <pbk0398@gmail.com>
2024-07-05 16:05:59 +0900

    feat: 유저 아이디 지정

--- src/main/java/com/MiTi/MiTi/entity/User.java
+++ src/main/java/com/MiTi/MiTi/entity/User.java
@@ -1,13 +1,13 @@
 package com.MiTi.MiTi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity(name = "User")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String user_id;
 }
