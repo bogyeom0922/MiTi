0382da17d5a04de4efea132682744ee9f97b6b74
bogyeom0922 <pbk0398@gmail.com>
2024-07-15 18:01:00 +0900

    feat: comment값 반환

--- src/main/java/com/MiTi/MiTi/service/CommentService.java
+++ src/main/java/com/MiTi/MiTi/service/CommentService.java
@@ -1,17 +1,24 @@
 package com.MiTi.MiTi.service;
 
+import com.MiTi.MiTi.dto.CommentDto;
 import com.MiTi.MiTi.entity.Comment;
 import com.MiTi.MiTi.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CommentService {
     @Autowired
-    private final CommentRepository commentRepository;
+    private CommentRepository commentRepository;
 
-    public CommentService(CommentRepository commentRepository) {
-        this.commentRepository = commentRepository;
+    public List<Comment> comments(String albumId) {
+        return commentRepository.findByAlbumId(albumId)
+                .stream() //리스트에 저장된 요소들 하나씩 참조하며 반복 처리할 때 사용
+                .map(CommentDto::createCommentDto)
+                .collect(Collectors.toList());
     }
 
     public void saveComment(Comment comment) {
