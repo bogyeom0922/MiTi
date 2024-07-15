14330bb073aeaf0ff9ad05266992d740ba40bf55
bogyeom0922 <pbk0398@gmail.com>
2024-07-15 16:02:10 +0900

    feat: 특정 앨범 댓글 조회 쿼리 작성

--- src/main/java/com/MiTi/MiTi/repository/CommentRepository.java
+++ src/main/java/com/MiTi/MiTi/repository/CommentRepository.java
@@ -2,6 +2,15 @@ package com.MiTi.MiTi.repository;
 
 import com.MiTi.MiTi.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    //특정 앨범 댓글 조회
+    @Query(value = "SELECT * FROM user_comment WHERE album_id = :albumId", nativeQuery = true)
+    List<Comment> findByAlbumId(Long albumId);
+
 }
