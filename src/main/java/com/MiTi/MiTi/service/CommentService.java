59b9928fa37df0aafbcf7a6e23c6db9fbce50841
bogyeom0922 <pbk0398@gmail.com>
2024-07-15 15:52:22 +0900

    feat: commentRespository 선언

--- src/main/java/com/MiTi/MiTi/service/CommentService.java
+++ src/main/java/com/MiTi/MiTi/service/CommentService.java
@@ -1,8 +1,11 @@
 package com.MiTi.MiTi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
+    @Autowired
+    private CommentRepository commentRepository;
 }
