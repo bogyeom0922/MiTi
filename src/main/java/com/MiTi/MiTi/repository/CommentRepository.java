83a216924cecc2c7492d7d65cf046f8381d2cc11
bogyeom0922 <pbk0398@gmail.com>
2024-07-17 16:01:04 +0900

    feat: 오류 수정

--- src/main/java/com/MiTi/MiTi/repository/CommentRepository.java
+++ src/main/java/com/MiTi/MiTi/repository/CommentRepository.java
@@ -10,7 +10,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 앨범 댓글 조회
     List<Comment> findByAlbumId(String albumId);
 
-    // 특정 사용자 댓글 조회
-    List<Comment> findByUserId(String userId);
-
 }
