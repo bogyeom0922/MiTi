619e2587b49d8400b16a55bc5e1bf28c2ea852c0
mimmimkim <mynseoo@gmail.com>
2024-07-22 15:44:37 +0900

    Your commit message

--- src/main/java/com/MiTi/MiTi/controller/RecordController.java
+++ src/main/java/com/MiTi/MiTi/controller/RecordController.java
@@ -5,6 +5,7 @@ import com.MiTi.MiTi.service.RecordService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -17,9 +18,9 @@ public class RecordController {
         this.recordService = recordService;
     }
 
-    @GetMapping("/mypage/record")
-    public String list(Model model) {
-        List<RecordDto> recordDtoList = recordService.getRecordList();
+    @GetMapping("/mypage/record/{user_id}")
+    public String list(@PathVariable Integer user_id, Model model) {
+        List<RecordDto> recordDtoList = recordService.getRecordListByUserId(user_id);
         model.addAttribute("postList", recordDtoList);
         return "mypage_record";
     }
--- src/main/java/com/MiTi/MiTi/repository/RecordRepository.java
+++ src/main/java/com/MiTi/MiTi/repository/RecordRepository.java
@@ -4,5 +4,9 @@ import com.MiTi.MiTi.entity.Record;
 import com.MiTi.MiTi.entity.RecordId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecordRepository extends JpaRepository<Record, RecordId> {
+
+    List<Record> findByUser_id(Integer user_id);
 }
