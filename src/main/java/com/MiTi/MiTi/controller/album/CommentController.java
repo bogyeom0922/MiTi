81ff833a19e1188ec7d2cf3a661c194d42b3a41a
bogyeom0922 <pbk0398@gmail.com>
2024-10-08 00:58:20 +0900

    fix: 댓글 오류 수정 서버 응답 형식 수정
    close: #69

--- src/main/java/com/MiTi/MiTi/controller/album/CommentController.java
+++ src/main/java/com/MiTi/MiTi/controller/album/CommentController.java
@@ -3,12 +3,17 @@ package com.MiTi.MiTi.controller.album;
 import com.MiTi.MiTi.dto.CommentDto;
 import com.MiTi.MiTi.entity.Comment;
 import com.MiTi.MiTi.service.CommentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class CommentController {
 
@@ -20,14 +25,18 @@ public class CommentController {
     }
 
     @PostMapping("/submitComment")
-    public ResponseEntity<String> submitComment(@RequestBody CommentDto commentDto) {
+    public ResponseEntity<Map<String, String>> submitComment(@RequestBody CommentDto commentDto) {
         Comment comment = new Comment(
                 commentDto.getAlbumId(),
                 commentDto.getProviderId(),
                 commentDto.getComment()
         );
         commentService.saveComment(comment);
-        return ResponseEntity.ok("Comment saved successfully");
+
+        // JSON 응답으로 반환
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Comment saved successfully");
+        return ResponseEntity.ok(response);
     }
 
 }
